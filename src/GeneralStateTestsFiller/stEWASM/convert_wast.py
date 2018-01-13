4cd415066d0327127f84c41673f08eb2a118281c
Jose Hugo De la cruz Romero <jhugodc@gmail.com>
2018-01-13 13:20:53 +0000

    new test cases, change script to show wat2wasm errors

--- src/GeneralStateTestsFiller/stEWASM/convert_wast.py
+++ src/GeneralStateTestsFiller/stEWASM/convert_wast.py
@@ -1,10 +1,15 @@
 import os, glob, sys, subprocess, yaml, pdb, json, binascii
 
-def wat2wasm(code):
+def wat2wasm(code, file_name):
   with open("tmp.wast", "w+") as f:
     f.write(code)
 
   result = subprocess.run(['./wat2wasm',  'tmp.wast', '-o', 'tmp.wasm'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+  if result.returncode != 0:
+    print(file_name)
+    print(result.stderr)
+    exit(1)
 
   with open('tmp.wasm', 'rb') as f:
     result = f.read()
@@ -24,11 +29,11 @@ def convert_state_test(file_name):
     # conver pre account code
     for addr in state_test[test_case]['pre']:
       if state_test[test_case]['pre'][addr]['code'] != ['']:
-        state_test[test_case]['pre'][addr]['code'] = wat2wasm(state_test[test_case]['pre'][addr]['code'])
+        state_test[test_case]['pre'][addr]['code'] = wat2wasm(state_test[test_case]['pre'][addr]['code'], file_name)
 
     if state_test[test_case]['transaction']['data'] != ['']:
       for i in range(0, len(state_test[test_case]['transaction']['data'])):
-        state_test[test_case]['transaction']['data'][i] = wat2wasm(state_test[test_case]['transaction']['data'][i])
+        state_test[test_case]['transaction']['data'][i] = wat2wasm(state_test[test_case]['transaction']['data'][i], file_name)
 
   with open(file_name.strip('.yml')+'.json', 'w') as f:
     f.write(json.dumps(state_test, indent=4, sort_keys=True))
