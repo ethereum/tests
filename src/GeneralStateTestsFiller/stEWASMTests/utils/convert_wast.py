39432e9d3f3dfb574f49bf14afb95ceeb688998f
Jared Wasinger <j-wasinger@hotmail.com>
2018-01-14 18:32:44 +0000

    fix getBalance, getCallDataSize

--- src/GeneralStateTestsFiller/stEWASMTests/utils/convert_wast.py
+++ src/GeneralStateTestsFiller/stEWASMTests/utils/convert_wast.py
@@ -28,14 +28,15 @@ def convert_state_test(file_name):
   for test_case in state_test.keys():
     # conver pre account code
     for addr in state_test[test_case]['pre']:
-      if state_test[test_case]['pre'][addr]['code'] != ['']:
+      if state_test[test_case]['pre'][addr]['code'] != '':
         state_test[test_case]['pre'][addr]['code'] = '0x'+wat2wasm(state_test[test_case]['pre'][addr]['code'], file_name)
 
-    if state_test[test_case]['transaction']['data'] != [''] and state_test[test_case]['transaction']['to'] == '':
+    if state_test[test_case]['transaction']['data'] != '' and state_test[test_case]['transaction']['to'] == '':
       for i in range(0, len(state_test[test_case]['transaction']['data'])):
         state_test[test_case]['transaction']['data'][i] = '0x'+wat2wasm(state_test[test_case]['transaction']['data'][i], file_name)
 
-  with open(file_name.strip('.yml')+'.json', 'w') as f:
+  new_fn = file_name.replace('.yml','.json')
+  with open(new_fn, 'w') as f:
     f.write(json.dumps(state_test, indent=4, sort_keys=True))
     
 for file_name in glob.glob('*.yml'):
