dccfad9b56d217082527561579bc2fa40dc50028
Lane Rettig <lanerettig@gmail.com>
2018-01-17 19:55:00 +0000

    Fix convert_wast to allow empty code field for EOAs

--- src/GeneralStateTestsFiller/stEWASMTests/utils/convert_wast.py
+++ src/GeneralStateTestsFiller/stEWASMTests/utils/convert_wast.py
@@ -28,7 +28,7 @@ def convert_state_test(file_name):
   for test_case in state_test.keys():
     # conver pre account code
     for addr in state_test[test_case]['pre']:
-      if state_test[test_case]['pre'][addr]['code'] != ['']:
+      if state_test[test_case]['pre'][addr]['code'] != '':
         state_test[test_case]['pre'][addr]['code'] = '0x'+wat2wasm(state_test[test_case]['pre'][addr]['code'], file_name)
 
     if state_test[test_case]['transaction']['data'] != [''] and state_test[test_case]['transaction']['to'] == '':
