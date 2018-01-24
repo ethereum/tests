dcada5236c823163604537f23fb46b743f58b187
Lane Rettig <lanerettig@gmail.com>
2018-01-24 17:27:57 +0100

    Adds createFromTransaction test filler
    Tests creating a contract from transaction data.
    Modify convert_wast.py to allow empty 'pre' data.
    
    Not currently working, may be due to
    https://github.com/ewasm/hera/issues/122.

--- src/GeneralStateTestsFiller/stEWASMTests/utils/convert_wast.py
+++ src/GeneralStateTestsFiller/stEWASMTests/utils/convert_wast.py
@@ -27,9 +27,10 @@ def convert_state_test(file_name):
 
   for test_case in state_test.keys():
     # conver pre account code
-    for addr in state_test[test_case]['pre']:
-      if state_test[test_case]['pre'][addr]['code'] != '':
-        state_test[test_case]['pre'][addr]['code'] = '0x'+wat2wasm(state_test[test_case]['pre'][addr]['code'], file_name)
+    if state_test[test_case].get('pre', None):
+        for addr in state_test[test_case]['pre']:
+          if state_test[test_case]['pre'][addr]['code'] != '':
+            state_test[test_case]['pre'][addr]['code'] = '0x'+wat2wasm(state_test[test_case]['pre'][addr]['code'], file_name)
 
     if state_test[test_case]['transaction']['data'] != '' and state_test[test_case]['transaction']['to'] == '':
       for i in range(0, len(state_test[test_case]['transaction']['data'])):
