9f3d5197c06edb9c6036f19b4f1d8fe3cb31af49
Ori Pomerantz <qbzzt1@gmail.com>
2021-10-18 21:02:50 +0000

    Added handling for geth failures

--- src/Templates/RandomTests/oneTest.sh
+++ src/Templates/RandomTests/oneTest.sh
@@ -4,6 +4,7 @@ suite=GeneralStateTests/stExample
 test=random
 dir=`pwd`/../../..
 
+
 # Create a random test
 ./randomTest.js > ../../GeneralStateTestsFiller/stExample/randomFiller.yml
 
@@ -12,6 +13,14 @@ cd ~
 # Run the random test on Geth
 ./dretesteth.sh -t $suite -- --testpath $dir --singletest $test \
                 --filltests --datadir /tests/config
+
+gethRetVal=$?
+
+if [ $gethRetVal -gt 0 ]; then
+  cp $dir/src/GeneralStateTestsFiller/stExample/randomFiller.yml \
+        /tmp/fail-on-geth-`date +%F:%H:%M:%S:%N`Filler.yml
+fi
+
 
 sleep 5   # Prevent race problems
 
@@ -23,7 +32,7 @@ besuRetVal=$?
 
 if [ $besuRetVal -gt 0 ]; then
   mv $dir/src/GeneralStateTestsFiller/stExample/randomFiller.yml \
-        /tmp/fail`date +%F:%H:%M:%S:%N`Filler.yml
+        /tmp/fail-on-besu-`date +%F:%H:%M:%S:%N`Filler.yml
 fi
 
 
