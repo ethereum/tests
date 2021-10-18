c2a45328e21dc108352f7ee683dcfddd11f16125
Ori Pomerantz <qbzzt1@gmail.com>
2021-10-18 22:01:29 +0000

    Made number and type command line options

--- src/Templates/RandomTests/oneTest.sh
+++ src/Templates/RandomTests/oneTest.sh
@@ -1,9 +1,14 @@
 #! /usr/bin/bash
 
+# Usage:
+# oneTest.sh <test type>
 suite=GeneralStateTests/stExample
 test=random
 dir=`pwd`/../../..
+outputDir=/tmp
+testType=$1
 
+echo Test type $testType
 
 # Create a random test
 ./randomTest.js > ../../GeneralStateTestsFiller/stExample/randomFiller.yml
@@ -11,30 +16,43 @@ dir=`pwd`/../../..
 cd ~
 
 # Run the random test on Geth
-./dretesteth.sh -t $suite -- --testpath $dir --singletest $test \
-                --filltests --datadir /tests/config
+if [ "$testType" == "block" ]
+then
+    ./dretesteth.sh -t $suite -- --testpath $dir --singletest $test \
+                --all --fillchain --filltests --datadir /tests/config
+else
+    ./dretesteth.sh -t $suite -- --testpath $dir --singletest $test \
+                --all --filltests --datadir /tests/config
+fi
 
 gethRetVal=$?
 
 if [ $gethRetVal -gt 0 ]; then
   cp $dir/src/GeneralStateTestsFiller/stExample/randomFiller.yml \
-        /tmp/fail-on-geth-`date +%F:%H:%M:%S:%N`Filler.yml
+        $outputDir/fail-on-geth-$testType-`date +%F:%H:%M:%S:%N`Filler.yml
+  exit 1
 fi
 
 
 sleep 5   # Prevent race problems
 
 # Run the same random test on Besu
-./dretesteth.sh -t $suite -- --testpath $dir --singletest $test \
-                --clients besu --datadir /tests/config
+# Run the random test on Geth
+if [ "$testType" == "block" ]
+then
+    ./dretesteth.sh -t BC$suite -- --testpath $dir --singletest $test \
+                --all --clients besu --datadir /tests/config
+else
+     ./dretesteth.sh -t $suite -- --testpath $dir --singletest $test \
+                --all --clients besu --datadir /tests/config
+fi
+
+
 
 besuRetVal=$?
 
 if [ $besuRetVal -gt 0 ]; then
   mv $dir/src/GeneralStateTestsFiller/stExample/randomFiller.yml \
-        /tmp/fail-on-besu-`date +%F:%H:%M:%S:%N`Filler.yml
+        $outputDir/fail-on-besu-$testType-`date +%F:%H:%M:%S:%N`Filler.yml
 fi
 
-
-
-# if [ $besuRetVal -eq 0 ]; then echo GOOD; fi
