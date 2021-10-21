40c8b357abc27a5eab55624a022027742e9e8619
Ori Pomerantz <qbzzt1@gmail.com>
2021-10-21 14:03:38 +0000

    Added an option to specify where to save the error files

--- src/Templates/RandomTests/oneTest.sh
+++ src/Templates/RandomTests/oneTest.sh
@@ -4,32 +4,32 @@
 # oneTest.sh <test type>
 suite=GeneralStateTests/stExample
 test=random
-dir=$2
-outputDir=/tmp
+testPath=$2
+errPath=$3
 testType=$1
 
 echo Test type $testType
 
 # Create a random test
-./randomTest.js > $dir/src/GeneralStateTestsFiller/stExample/${test}Filler.yml
+./randomTest.js > $testPath/src/GeneralStateTestsFiller/stExample/${test}Filler.yml
 
 cd ~
 
 # Run the random test on Geth
 if [ "$testType" == "block" ]
 then
-    ./dretesteth.sh -t $suite -- --testpath $dir --singletest $test \
+    ./dretesteth.sh -t $suite -- --testpath $testPath --singletest $test \
                 --all --fillchain --filltests --datadir /tests/config
 else
-    ./dretesteth.sh -t $suite -- --testpath $dir --singletest $test \
+    ./dretesteth.sh -t $suite -- --testpath $testPath --singletest $test \
                 --all --filltests --datadir /tests/config
 fi
 
 gethRetVal=$?
 
 if [ $gethRetVal -gt 0 ]; then
-  cp $dir/src/GeneralStateTestsFiller/stExample/randomFiller.yml \
-        $outputDir/fail-on-geth-$testType-`date +%F:%H:%M:%S:%N`Filler.yml
+  mv $testPath/src/GeneralStateTestsFiller/stExample/randomFiller.yml \
+        $errPath/fail-on-geth-$testType-`date +%F:%H:%M:%S:%N`Filler.yml
   exit 1
 fi
 
@@ -40,10 +40,10 @@ sleep 5   # Prevent race problems
 # Run the random test on Geth
 if [ "$testType" == "block" ]
 then
-    ./dretesteth.sh -t BC$suite -- --testpath $dir --singletest $test \
+    ./dretesteth.sh -t BC$suite -- --testpath $testPath --singletest $test \
                 --all --clients besu --datadir /tests/config
 else
-     ./dretesteth.sh -t $suite -- --testpath $dir --singletest $test \
+     ./dretesteth.sh -t $suite -- --testpath $testPath --singletest $test \
                 --all --clients besu --datadir /tests/config
 fi
 
@@ -52,7 +52,7 @@ fi
 besuRetVal=$?
 
 if [ $besuRetVal -gt 0 ]; then
-  mv $dir/src/GeneralStateTestsFiller/stExample/randomFiller.yml \
-        $outputDir/fail-on-besu-$testType-`date +%F:%H:%M:%S:%N`Filler.yml
+  mv $testPath/src/GeneralStateTestsFiller/stExample/randomFiller.yml \
+        $errPath/fail-on-besu-$testType-`date +%F:%H:%M:%S:%N`Filler.yml
 fi
 
--- src/Templates/RandomTests/runTests.sh
+++ src/Templates/RandomTests/runTests.sh
@@ -5,34 +5,35 @@
 numTests=0
 testType=state
 testPath=`pwd`/../../..
+errPath=/tmp
 
-while getopts n:bt: opt
+while getopts n:bt:e: opt
 do
   case $opt in
   n) numTests=$OPTARG;;
   b) testType=block;;
   t) testPath=$OPTARG;;
-  ?) printf "Usage: %s: [-n <num>] [-b] [-t <path>]\n" $0
+  e) errPath=$OPTARG;
+  ?) printf "Usage: %s: [-n <num>] [-b] [-t <path>] [-e <path>]\n" $0
      printf "            -n <number of tests>, default infinite\n"
      printf "            -b Blockchain test (default state test)\n"
      printf "            -t Path to tests repository\n"
+     printf "            -e Path to save errors\n"
      exit 2;;
   esac
 done
-
-echo $testPath
 
 if [ $numTests -eq 0 ]
 then
     while true
     do
-        ./oneTest.sh $testType $testPath
+        ./oneTest.sh $testType $testPath $errPath
     done
 else
     for run in $(seq $numTests);
     do
        echo "Test #$run"
-       ./oneTest.sh $testType $testPath
+       ./oneTest.sh $testType $testPath $errPath
     done
 fi
 
