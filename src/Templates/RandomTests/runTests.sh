ece37b88430e164664e26c24d75e184fe1972d91
Ori Pomerantz <qbzzt1@gmail.com>
2021-10-25 18:56:30 +0000

    Added the command line options requested by Dimitry

--- src/Templates/RandomTests/runTests.sh
+++ src/Templates/RandomTests/runTests.sh
@@ -7,18 +7,20 @@ testType=state
 testPath=`pwd`/../../..
 errPath=/tmp
 
-while getopts n:bt:e: opt
+while getopts n:bt:e:r opt
 do
   case $opt in
   n) numTests=$OPTARG;;
   b) testType=block;;
+  r) nodocker=true;;
   t) testPath=$OPTARG;;
-  e) errPath=$OPTARG;
-  ?) printf "Usage: %s: [-n <num>] [-b] [-t <path>] [-e <path>]\n" $0
+  e) errPath=$OPTARG;;
+  ?) printf "Usage: %s: [-n <num>] [-b] [-t <path>] [-e <path>] [-r]\n" $0
      printf "            -n <number of tests>, default infinite\n"
      printf "            -b Blockchain test (default state test)\n"
      printf "            -t Path to tests repository\n"
-     printf "            -e Path to save errors\n"
+     printf "            -e Path to save error files\n"
+     printf "            -r Use retesteth, not a docker container\n"
      exit 2;;
   esac
 done
@@ -27,13 +29,13 @@ if [ $numTests -eq 0 ]
 then
     while true
     do
-        ./oneTest.sh $testType $testPath $errPath
+        ./oneTest.sh $testType $testPath $errPath $nodocker
     done
 else
     for run in $(seq $numTests);
     do
        echo "Test #$run"
-       ./oneTest.sh $testType $testPath $errPath
+       ./oneTest.sh $testType $testPath $errPath $nodocker
     done
 fi
 
